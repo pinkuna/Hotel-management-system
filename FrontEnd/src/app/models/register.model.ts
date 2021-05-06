6a80b4f9d1dcaaa47dcaf138cdf01cfb5c218394
Jetniphan Pukkham <jeni8399@gmail.com>
2021-05-06 16:57:00 +0700

    add networkservice

--- FrontEnd/src/app/models/register.model.ts
+++ FrontEnd/src/app/models/register.model.ts
@@ -9,3 +9,15 @@ export class register {
     email: any;
     address : string;
 }
+
+export interface registerRequire {
+    username: string;
+    Usesurname: string;
+    password: string;
+    repassword: string;
+    Usename: string;
+    idcard: number;
+    phonNum: number;
+    email: any;
+    address : string;
+}
---
+++ FrontEnd/src/app/services/network-user.service.ts
@@ -0,0 +1,32 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { register, registerRequire } from '../models/register.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class NetworkUserService {
+
+  constructor(private httpClient: HttpClient) { }
+
+  postRegister(Register: register): Observable<registerRequire>{
+    return this.httpClient.post<registerRequire>('http://localhost:1150/register',this.makeFormData(Register))
+
+  }
+
+  makeFormData(Register: register): FormData{
+    const formData = new FormData()
+    formData.append('username',Register.username)
+    formData.append('Usesurname',Register.Usesurname)
+    formData.append('password',Register.password)
+    formData.append('repassword',Register.repassword)
+    formData.append('Usename',Register.Usename)
+    formData.append('idcard',`${Register.idcard}`)
+    formData.append('phonNum',`${Register.phonNum}`)
+    formData.append('email',Register.email)
+    formData.append('address',Register.address)
+
+    return formData;
+  }
+}
