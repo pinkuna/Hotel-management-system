349376ac12ef4a9cb2dfe7aa8879170c21f165d6
Jetniphan Pukkham <jeni8399@gmail.com>
2021-05-07 13:57:37 +0700

    add login netwoek

--- FrontEnd/src/app/services/network-user.service.ts
+++ FrontEnd/src/app/services/network-user.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { login } from '../models/login.model';
 import { register, registerRequire } from '../models/register.model';
 
 @Injectable({
@@ -10,7 +11,11 @@ export class NetworkUserService {
 
   constructor(private httpClient: HttpClient) { }
 
-  postRegister(Register: register): Observable<any>{
-    return this.httpClient.post<any>('http://localhost:8004/api/register',Register)
+  postRegister(Register: register): Observable<any> {
+    return this.httpClient.post<any>('http://localhost:8004/api/register', Register)
+  }
+
+  postlogin(Login: login): Observable<any> {
+    return this.httpClient.post<any>('http://localhost:8004/api/login', Login)
   }
 }
