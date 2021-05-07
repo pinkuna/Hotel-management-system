715721a2a3103c8253dadea8caa1c4a626b24232
phalapat <palapatmc@gmail.com>
2021-05-08 02:22:43 +0700

    update condition login and hash password

--- backend/NEW/controllers/login.js
+++ backend/NEW/controllers/login.js
@@ -19,40 +19,6 @@ const pool = new pg.Pool({
     port: 5432,
 });
 
-// router.post("/", function(request, response) {
-//     var username = request.body.username;
-//     var password = request.body.password;
-//     if (username && password) {
-//         pool.connect(function(err, client, done) {
-//             const queryMessage = `SELECT * FROM register WHERE username = '${username}' AND password = '${password}'`;
-//             if (err) {
-//                 return console.error("connection error", err);
-//             }
-//             client.query(queryMessage, function(err, result) {
-//                 const data = result.rows;
-//                 client.end();
-//                 if (data.length > 0) {
-//                     request.session.loggedin = true;
-//                     request.session.username = username;
-//                     request.session.userid = data[0].id;
-//                     response.status(200).json({ status: 'success', data: 'login completed' });
-//                     //response.status(200).redirect('/home');
-//                 } else {
-//                     request.session.loggedin = false;
-//                     response.status(200).json({ status: 'failed', data: 'username or password invalid' });
-//                     //response.status(200).redirect('/login');
-//                 }
-//                 response.end();
-//             });
-//         });
-//     } else {
-//         request.session.loggedin = false;
-//         response.status(200).json({ status: 'failed', data: 'please enter Username and password' });
-//         response.end();
-//     }
-// });
-
-
 router.post("/", (request, response) => {
     var username = request.body.username;
     var password = request.body.password;
