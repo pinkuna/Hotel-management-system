49b00c5deb8f224beb091de03a0ca5796e38cc7f
Jetniphan Pukkham <jeni8399@gmail.com>
2021-05-09 01:00:44 +0700

    insert interface response, get/amin/use, post to get in backend

--- FrontEnd/src/app/models/Respones.model.ts
+++ FrontEnd/src/app/models/Respones.model.ts
@@ -2,3 +2,32 @@ export interface Response {
     data: string;
     status: string;
 }
+
+export interface BookingRes {
+    id: number;
+    loginid: number;
+    idcard: string;
+    name: string;
+    phonenum: number;
+    email: string;
+    date: Date;
+}
+
+export interface ReportRes {
+    id: number;
+    roomnum: number;
+    name: string;
+    phonenum: number;
+    theproblems: string;
+    requre: string;
+    title: string;
+}
+
+export interface CheckoutRes {
+    id: number;
+    roomnum: number;
+    name: string;
+    phonenum: number;
+    date: Date;
+}
+
