db8d1dbce02d41b6b6c8b7594bae04b4cf304a91
phalapat <palapatmc@gmail.com>
2021-05-09 15:37:39 +0700

    edit register

--- backend/NEW/controllers/register.js
+++ backend/NEW/controllers/register.js
@@ -23,39 +23,76 @@ const pool = new pg.Pool({
 });
 
 
-router.post("/", (request, response) => {
-    if (request.session.loggedin) {
-        let reposts = {
-            text: `insert into report(roomNum, name, phoneNum, theProblems, Requre, title) values ($1, $2, $3, $4, $5, $6);`,
-            values: [
-                request.body.roomNum,
-                request.body.name,
-                request.body.phoneNum,
-                request.body.theProblems,
-                request.body.Requre,
-                request.body.title
-            ],
-        };
-        pool.connect((err, client, done) => {
-            if (err) {
-                return console.error("connexion error", err);
-            }
-            client.query(reposts, function(err, result) {
-                done();
+// router.post("/", (request, response) => {
+//     if (request.session.loggedin) {
+//         let reposts = {
+//             text: `insert into report(roomNum, name, phoneNum, theProblems, Requre, title) values ($1, $2, $3, $4, $5, $6);`,
+//             values: [
+//                 request.body.roomNum,
+//                 request.body.name,
+//                 request.body.phoneNum,
+//                 request.body.theProblems,
+//                 request.body.Requre,
+//                 request.body.title
+//             ],
+//         };
+//         pool.connect((err, client, done) => {
+//             if (err) {
+//                 return console.error("connexion error", err);
+//             }
+//             client.query(reposts, function(err, result) {
+//                 done();
 
-                if (err) {
-                    return console.error("error running query", err);
-                }
-            });
-        });
-        response.status(201).json({ status: 'success', data: 'report send to admin' });
-        response.end();
-    } else {
-        response.status(200).json({ status: 'failed', data: 'please login' });
-        //response.redirect('/api/login/');
-        response.end();
+//                 if (err) {
+//                     return console.error("error running query", err);
+//                 }
+//             });
+//         });
+//         response.status(201).json({ status: 'success', data: 'report send to admin' });
+//         response.end();
+//     } else {
+//         response.status(200).json({ status: 'failed', data: 'please login' });
+//         //response.redirect('/api/login/');
+//         response.end();
+//     }
+// });
+
+// path = localhost:8004/api/register
+router.post('/', (req, res) => {
+    let hash_password = bcrypt.hashSync(req.body.password, 10)
+
+    let queryMessage = {
+        text: `insert into Register(   
+            username,
+            usersurname,
+            password,
+            Usename,
+            idcard,
+            phoneNum,
+            email,
+            address
+        ) values ($1, $2, $3, $4, $5, $6, $7, $8);`,
+        values: [req.body.username, req.body.usersurname, hash_password,
+        req.body.Usename, req.body.idcard, req.body.phoneNum,
+        req.body.email, req.body.address
+        ]
     }
-});
+    console.log(pool.totalCount)
+    pool.connect(function (err, client, done) {
+        if (err) {
+            return console.error('connection error', err);
+        }
+        client.query(queryMessage, function (err, result) {
+            done();  // call `done()` to release the client back to the pool
+            if (err) {
+                return console.error('error running query', err);
+            } else {
+            }
+        })
+    })
+    res.status(201).json({ status: 'success', data: 'Register success' })
+    res.end()
+})
 
 
 router.get("/admin", (request, response) => {
